218570c855401b0903bf45b4c83d85ecd50cd72f
Tahmid3539 <109985853+Tahmid3539@users.noreply.github.com>
2022-08-15 22:47:41 +0600

    Add files via upload

--- db_connect.php
+++ db_connect.php
@@ -1,17 +1,19 @@
 <?php 
-function dbConnect(){
+function db_conn()
+{
+    $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "product info";
-    $server = "localhost";
+    $dbname = "foodie";
 
     try {
-        $conn = new PDO('mysql:host='.$server.';dbname='.$dbname.';charset=utf8', $username, $password);
+        $conn = new PDO('mysql:host='.$servername.';dbname='.$dbname.';charset=utf8', $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        // var_dump($conn) ;
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
     return $conn;
 }
-
 ?>
